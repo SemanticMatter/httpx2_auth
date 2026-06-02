35cb69db884826c9044c8a9e2f779f16d79cf38c
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2026-06-02 14:09:18 +0200

    Make version a major minor patch (#3)
    * Remove batches from README
    
    * Use version 2.0.0

--- httpx2_auth/__init__.py
+++ httpx2_auth/__init__.py
@@ -83,6 +83,5 @@ __all__ = [
 # Major should be incremented in case there is a breaking change. (eg: 2.5.8 -> 3.0.0)
 # Minor should be incremented in case there is an enhancement. (eg: 2.5.8 -> 2.6.0)
 # Patch should be incremented in case there is a bug fix. (eg: 2.5.8 -> 2.5.9)
-
-# Started epoch 2 to be able to use distinct version numbers for the first release of httpx2_auth, which is compatible with httpx2.
-__version__ = "2.0.1.0"
+# NOTE: Using major version 2 as of releasing `httpx2-auth`.
+__version__ = "2.0.0"
